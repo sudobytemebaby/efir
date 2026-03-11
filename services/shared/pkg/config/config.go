1eee1cb0bfe20d086d9ab706130435d231be4c66
sudobytemebaby <i027356@inbox.ru>
2026-03-11 17:47:02 +0700

    chore(shared): add config package with cleanenv
    - implements Load() to read config from environment variables
    - uses cleanenv for env-required and env-default tags
    - provides base Config struct with common fields
    - adds unit tests

---
+++ services/shared/pkg/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	GRPCPort    string `env:"GRPC_PORT" env-default:"50051"`
+	PostgresDSN string `env:"POSTGRES_DSN" env-required:"true"`
+	ValkeyAddr  string `env:"VALKEY_ADDR" env-default:"valkey:6379"`
+	ValkeyPass  string `env:"VALKEY_PASSWORD"`
+	NATSURL     string `env:"NATS_URL" env-default:"nats://nats:4222"`
+	JWTSecret   string `env:"JWT_SECRET" env-required:"true"`
+	AccessTTL   string `env:"JWT_ACCESS_TTL" env-default:"15m"`
+	RefreshTTL  string `env:"JWT_REFRESH_TTL" env-default:"168h"`
+}
+
+func Load(cfg *Config) error {
+	err := cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return errors.New("failed to read config: " + err.Error())
+	}
+
+	return nil
+}
